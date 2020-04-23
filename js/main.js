b0b6e9d50be8a66f556d25f5b4978e1b83322ded
Atiye <aty.sokhansanj@gmail.com>
2020-04-23 06:32:55 +0430

    git add .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 // $(function(){
 //     $('li').css('color','red');
 // })
+
+let navLink = document.querySelector('.nav-link');
+
+navLink.addEventListener('mouseleave', function (e) {
+   navLink.style='border-0'
+})
